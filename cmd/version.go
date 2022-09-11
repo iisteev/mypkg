5c7f55b4a6b55f93f5accf640b727cf9279c8ebc
iisteev <25986078+iisteev@users.noreply.github.com>
2022-09-11 19:13:21 +0200

    update year in the license header

--- cmd/version.go
+++ cmd/version.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 Isteevan Shetoo <isteevan.shetoo@is-info.fr>
+Copyright © 2022 Isteevan Shetoo <isteevan.shetoo@is-info.fr>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
